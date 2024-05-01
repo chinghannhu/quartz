227004caf2da591e7991b569dc504c714d0eb4fb
chinghannhu <chinghann.hu@gmail.com>
2024-05-01 22:46:51 +0800

    Quartz sync: May 1, 2024, 10:46 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Ching-Hann Hu",
+    pageTitle: "ching-hann hu",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
